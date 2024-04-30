df2ec1c59d5453f1b9cb8e03b67181aa8891e58d
Sterling <sterlingbarton81@gmail.com>
2024-04-29 21:26:22 -0500

    added about page, updated links in header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,6 @@ import Image from 'next/image';
 
 import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
-// import logoLaravel from '@/images/logos/laravel.svg';
-// import logoMirage from '@/images/logos/mirage.svg';
-// import logoStatamic from '@/images/logos/statamic.svg';
-// import logoStaticKit from '@/images/logos/statickit.svg';
-// import logoTransistor from '@/images/logos/transistor.svg';
-// import logoTuple from '@/images/logos/tuple.svg';
 
 export function Hero() {
   return (
@@ -31,7 +25,7 @@ export function Hero() {
         where collegiate athletes are made.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="/register">About Us</Button>
+        <Button href="/about">About Us</Button>
         <Button
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           variant="outline"
@@ -39,41 +33,6 @@ export function Hero() {
           Teams
         </Button>
       </div>
-      {/* <div className="mt-36 lg:mt-44">
-        <p className="font-display text-base text-slate-900">
-          Trusted by these six companies so far
-        </p>
-        <ul
-          role="list"
-          className="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
-        >
-          {[
-            [
-              { name: 'Transistor', logo: logoTransistor },
-              { name: 'Tuple', logo: logoTuple },
-              { name: 'StaticKit', logo: logoStaticKit },
-            ],
-            [
-              { name: 'Mirage', logo: logoMirage },
-              { name: 'Laravel', logo: logoLaravel },
-              { name: 'Statamic', logo: logoStatamic },
-            ],
-          ].map((group, groupIndex) => (
-            <li key={groupIndex}>
-              <ul
-                role="list"
-                className="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
-              >
-                {group.map((company) => (
-                  <li key={company.name} className="flex">
-                    <Image src={company.logo} alt={company.name} unoptimized />
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      </div> */}
     </Container>
   );
 }
