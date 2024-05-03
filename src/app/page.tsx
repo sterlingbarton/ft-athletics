dfa5fb5d11bb2afe0f2f8a1ab794fcd84aa2d1ce
Sterling <sterlingbarton81@gmail.com>
2024-05-03 11:00:43 -0500

    added carouselto home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,19 @@ import { Pricing } from '@/components/Pricing';
 import { PrimaryFeatures } from '@/components/PrimaryFeatures';
 import { SecondaryFeatures } from '@/components/SecondaryFeatures';
 import { Testimonials } from '@/components/Testimonials';
+import Carousel from '@/components/Carousel';
 
 export default function Home() {
+  const images = [
+    'src/images/background-auth.jpg',
+    'src/images/background-call-to-action.jpg',
+    'src/images/background-faqs.jpg',
+  ];
   return (
     <>
       <main>
         <Hero />
+        <Carousel images={images} />
         <PrimaryFeatures />
         <SecondaryFeatures />
         <CallToAction />
