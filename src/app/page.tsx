80f75f31f2e06ca83a279fffe33991cdddc02b8b
Sterling <sterlingbarton81@gmail.com>
2024-05-08 18:08:11 -0500

    removed extra carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import { CallToAction } from "@/components/CallToAction";
-import { Faqs } from "@/components/Faqs";
-import { Hero } from "@/components/Hero";
-import { Pricing } from "@/components/Pricing";
-import { PrimaryFeatures } from "@/components/PrimaryFeatures";
-import { SecondaryFeatures } from "@/components/SecondaryFeatures";
-import { Testimonials } from "@/components/Testimonials";
-import Carousel from "@/components/Carousel";
+import { CallToAction } from '@/components/CallToAction';
+import { Faqs } from '@/components/Faqs';
+import { Hero } from '@/components/Hero';
+import { Pricing } from '@/components/Pricing';
+import { PrimaryFeatures } from '@/components/PrimaryFeatures';
+import { SecondaryFeatures } from '@/components/SecondaryFeatures';
+import { Testimonials } from '@/components/Testimonials';
 
 export default function Home() {
-  const images = ["/hero-1.png", "/hero-2.png", "/hero-3.png"];
+  const images = ['/hero-1.png', '/hero-2.png', '/hero-3.png'];
   return (
     <>
       <main>
         <Hero />
-        {/* <Carousel images={images} /> */}
         <PrimaryFeatures />
         <SecondaryFeatures />
         <CallToAction />
