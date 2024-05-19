7c5681eee29f13d92a404cebaa997d3f513950ec
christian morales <christianmorales@christians-MacBook-Pro.local>
2024-05-19 11:44:58 -0500

    new

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -12,7 +12,7 @@ export function CallToAction() {
     >
       <div className="bg-white">
         <div className="overflow-hidden pt-32 sm:pt-14">
-          <div className="bg-gray-800">
+          <div className="bg-teal-900">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="relative pb-16 pt-48 sm:pb-24">
                 <div>
@@ -20,13 +20,18 @@ export function CallToAction() {
                     id="sale-heading"
                     className="text-4xl font-bold tracking-tight text-white md:text-5xl"
                   >
-                    Final Stock.
+                    First Triumph Athletics
                     <br />
-                    Up to 50% off.
+                    Houston, Texas
                   </h2>
-                  <div className="mt-6 text-base">
-                    <a href="#" className="font-semibold text-white">
-                      Shop the sale
+                  <div
+                    className="mt-6 text-base"
+                    style={{
+                      color: "#FBA603ff",
+                    }}
+                  >
+                    <a href="#" className="font-semibold">
+                      Learn More
                       <span aria-hidden="true"> &rarr;</span>
                     </a>
                   </div>
