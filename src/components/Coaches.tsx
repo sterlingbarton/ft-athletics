ae9e75be8f4b778d3f5f3452d4ee7408e8396249
Sterling <sterlingbarton81@gmail.com>
2024-05-20 21:02:57 -0500

    updated coaches component

--- src/components/Coaches.tsx
+++ src/components/Coaches.tsx
@@ -10,18 +10,13 @@ const team = [
   // More people...
 ];
 
-function Team() {
+function Coaches() {
   return (
     <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-48 lg:px-8">
       <div className="mx-auto max-w-2xl lg:mx-0">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Our team
+        <h2 className="text-3xl font-bold tracking-tight text-green-500 sm:text-4xl">
+          Our coaches
         </h2>
-        <p className="mt-6 text-lg leading-8 text-gray-600">
-          Sit facilis neque ab nulla vel. Cum eos in laudantium. Temporibus eos
-          totam in dolorum. Nemo vel facere repellendus ut eos dolores
-          similique.
-        </p>
       </div>
       <ul
         role="list"
@@ -45,4 +40,4 @@ function Team() {
   );
 }
 
-export default Team;
+export default Coaches;
